a648164323ac17af441a2a03eef1812dcb193b3d
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-09-09 13:31:00 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
-    <h1>DS metas</h1>
+    <>
+      <h1>DS metas</h1>
+      <NotificationButton/>
+    </>
   )
 }
 
